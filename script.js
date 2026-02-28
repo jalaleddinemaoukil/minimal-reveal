1b4ec5f351ab7b0c14f6c1b753ae2724a2f6a0a8
jalaleddinemaoukil <jalaleddinemaoukil@gmail.com>
2026-02-28 20:31:50 +0000

    Title reveal

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 gsap.registerPlugin(SplitText, ScrollTrigger);
+
+new SplitText(element, {
+  type: "words, chars",
+
+  autoSplit: true,
+
+  mask: "chars",
+
+  charClass: "char",
+
+  onSplit: (self) => {
+    gsap.from(self.elements, {
+      opacity: 1,
+    });
+
+    return gsap.from(self.chars, {
+      duration: 1,
+      yPercent: -120,
+      scale: 1.2,
+      stagger: 0.01,
+      ease: "expo.out",
+      scrollTrigger: {
+        trigger: self.element[0],
+        start: "top 88%",
+        once: true,
+      },
+    });
+  },
+});
